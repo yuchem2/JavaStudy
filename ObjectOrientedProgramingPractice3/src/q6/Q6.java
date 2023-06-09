e6025cbf839593218bd5257ca98b07064b8983dc
Yoon Jaehyun <yuchem2@gmail.com>
2023-06-09 22:40:08 +0900

    Add files via upload

---
+++ ObjectOrientedProgramingPractice3/src/q6/Q6.java
@@ -0,0 +1,16 @@
+package q6;
+
+/**
+ * Q6: Use Open Source
+ * 
+ * @author JaeHyun Yoon
+ * @since 06-09-2023
+ * @see Notepad
+ */
+public class Q6 {
+
+	public static void main(String[] args) {
+		new Notepad();
+	}
+
+}
